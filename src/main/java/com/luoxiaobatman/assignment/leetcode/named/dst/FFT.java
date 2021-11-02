2632698c1d9aa9dbb848f1f6bb932f02b1dd31ae
luoxiaobatman <luoxiaobatman@gmail.com>
2021-11-02 17:45:10 +0800

    tbc

---
+++ src/main/java/com/luoxiaobatman/assignment/leetcode/named/dst/FFT.java
@@ -0,0 +1,9 @@
+package com.luoxiaobatman.assignment.leetcode.named.dst;
+
+/**
+ * 快速傅里叶变换实现
+ *
+ * divide-and-conquer
+ */
+public class FFT {
+}
---
+++ src/main/java/com/luoxiaobatman/assignment/leetcode/named/dst/Naive.java
@@ -0,0 +1,10 @@
+package com.luoxiaobatman.assignment.leetcode.named.dst;
+
+/**
+ * 离散傅里叶变换
+ * 对一段有限长的离散信号，找出它含有的各个频率的正弦波分量。
+ *
+ * naive实现
+ */
+public class Naive {
+}
