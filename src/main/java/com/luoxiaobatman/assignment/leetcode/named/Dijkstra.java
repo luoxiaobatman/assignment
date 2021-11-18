805636d3ab7e283c5035e0d7d2e6fd18148dca63
luoxiaobatman <luoxiaobatman@gmail.com>
2021-11-18 11:30:27 +0800

    tbc

---
+++ src/main/java/com/luoxiaobatman/assignment/leetcode/named/Dijkstra.java
@@ -0,0 +1,11 @@
+package com.luoxiaobatman.assignment.leetcode.named;
+
+
+/**
+ * Graph Alg
+ * BFS -> DFS -> ShortestPath -> TopoSort -> CC -> SCC
+ * All of them should be trivial
+ *
+ */
+public class Dijkstra {
+}
